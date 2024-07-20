e1172e375aa9374acb9466688f43effaef510d59
Baguette <94969176+Not-Baguette@users.noreply.github.com>
2024-07-20 07:06:55 +0700

    optimizing + fixing for .exe release

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import os
 import shutil
 import platform
-import subprocess
+import sys
 
 def pull_latest_release(url: str, filename: str, arch_select: str = "") -> bool:
     """
@@ -16,24 +16,19 @@ def pull_latest_release(url: str, filename: str, arch_select: str = "") -> bool:
 
         if arch_select == "":
             asset_url = data["assets"][0]["browser_download_url"]  # Gets the latest release asset URL
-
-        elif arch_select == "64bit":
-            # find one that ends with "-64-bit.exe"
-            asset_url = data["assets"]
-            for asset in asset_url:
-                if asset["name"].endswith("-64-bit.exe"):
-                    asset_url = asset["browser_download_url"]
-                    break
-        
-        elif arch_select == "32bit":
-            # find one that ends with "-32-bit.exe"
-            asset_url = data["assets"]
-            for asset in asset_url:
-                if asset["name"].endswith("-32-bit.exe"):
-                    asset_url = asset["browser_download_url"]
-                    break
         else:
-            raise NotImplementedError(f"Unsupported architecture: {arch_select}")
+            if arch_select == "64bit":
+                filter = "-64-bit.exe"
+            
+            elif arch_select == "32bit":
+                filter = "-32-bit.exe"
+
+            # Find it depending on the architecture
+            asset_url = data["assets"]
+            for asset in asset_url:
+                if asset["name"].endswith(filter):
+                    asset_url = asset["browser_download_url"]
+                    break
             
         print(f"Asset URL found: {asset_url}")
         
@@ -48,7 +43,7 @@ def pull_latest_release(url: str, filename: str, arch_select: str = "") -> bool:
     except Exception as e:
         raise NotImplementedError(f"Failed to download {filename}: {e}")
 
-def install_mediatek_drivers(asset_url: str, filename: str) -> bool:
+def install_mediatek_drivers(asset_url: str, filename: str, app_path: str) -> bool:
     """
     Installs the MTK Preloader Driver
     """
@@ -63,7 +58,7 @@ def install_mediatek_drivers(asset_url: str, filename: str) -> bool:
         print("Unzipping & Installing drivers...")
         # Unzip the drivers & run the install drivers bat file
         shutil.unpack_archive("mtk_preloader_driver.zip", "mtk_preloader_driver")
-        abs_path =(os.path.join(os.path.dirname(__file__), "mtk_preloader_driver", 
+        abs_path =(os.path.join(app_path, "mtk_preloader_driver", 
                                 "Mediatek Driver Auto Installer v1.1352"))
         # cd to the dir first so the bat file can find required files
         os.system(f'cd "{abs_path}" && "Install Drivers.bat"')  
@@ -101,22 +96,30 @@ def main():
                  "git": "https://api.github.com/repos/git-for-windows/git/releases/latest",
                  "mtk_preloader_driver.zip": "https://androiddatahost.com/wp-content/uploads/Mediatek_Driver_Auto_Installer_v1.1352.zip"}
     
+    # Check if the script is running as an executable or as a python script, and changes the path accordingly
+    if getattr(sys, 'frozen', False):
+        app_path = os.path.dirname(sys.executable)
+    else:
+        app_path = os.path.dirname(__file__)
+
+
     "Installation successful." if install_needed_packages(path_dict) else "Installation failed." # Installs WinFSP, usbdk
-    "Installation successful." if install_mediatek_drivers(path_dict["mtk_preloader_driver.zip"], "mtk_preloader_driver.zip") else "Installation failed." # Install MTK Preloader Driver
+    "Installation successful." if install_mediatek_drivers(path_dict["mtk_preloader_driver.zip"], "mtk_preloader_driver.zip", app_path) else "Installation failed." # Install MTK Preloader Driver
     
     # Install git
-    if platform.architecture()[0] == "64bit":
+    arch = platform.architecture()[0]
+    if arch == "64bit":
         print("64-bit detected")
-        pull_latest_release("https://api.github.com/repos/git-for-windows/git/releases/latest", "git_inst.exe", arch_select="64bit")
-    elif platform.architecture()[0] == "32bit":
+    elif arch == "32bit":
         print("32-bit detected")
-        pull_latest_release("https://api.github.com/repos/git-for-windows/git/releases/latest", "git_inst.exe", arch_select="32bit")
     else:
         raise NotImplementedError("Failed to detect architecture")
     
+    pull_latest_release("https://api.github.com/repos/git-for-windows/git/releases/latest", "git_inst.exe", arch_select=arch)
+    
     # Start git installer
     print("Please follow the Installation wizard that has popped up on your screen")
-    os.system(f'"{os.path.dirname(__file__)}\\git_inst.exe"')
+    os.system(f'"{app_path}\\git_inst.exe"')
     input("Press any key to continue...")
     
     # Download and unzip mtkclient
@@ -127,12 +130,12 @@ def main():
             print("Please install Visual Studio Code from https://visualstudio.microsoft.com/visual-cpp-build-tools and then download 'Desktop Development with C++'")
     except OSError:
         pass
-    # Open mtkclient
+    # Open mtkclient, avoid using popen here.
     os.system("cd mtkclient && start . && start cmd.exe")
 
     print("-" * os.get_terminal_size().columns)
     print("Installation successful, you may close this window now.".center(os.get_terminal_size().columns))
-    print("-" * os.get_terminal_size().columns)
+    input("-" * os.get_terminal_size().columns) # use input here to avoid closing the window on .exe version
 
 if __name__ == "__main__":
     text_list = ["Mtkclient Installer", "By: Baguette", "Version: 1.1"]
